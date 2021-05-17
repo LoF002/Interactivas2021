d98b17d377c5b0eb308fbcd89d5e435ac77b1a1d
Luis Fernando Espinoza Mora <71098673+LoF002@users.noreply.github.com>
2021-05-17 04:24:19 -0600

    laboratorio y avance de proyecto
    el primer laboratorio del curso y un avance del frontend mobile del proyecto final

---
+++ eventos/js/main.js
@@ -0,0 +1,10 @@
+function openMobileMenu(){
+    let nav = document.body.getElementsByClassName('top-nav')[0];
+    nav.style.left="0";
+    
+}//Fin funtion
+
+function closeMobileMenu(){
+    let nav = document.body.getElementsByClassName('top-nav')[0];
+    nav.style.left="-24em";
+}
