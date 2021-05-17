204eb7fff6df3b0be666881d4a0ec3e89ef57422
Luis Fernando Espinoza Mora <71098673+LoF002@users.noreply.github.com>
2021-05-17 09:39:33 -0600

    Proyecto Eventos
    Avances

--- eventos/js/main.js
+++ eventos/js/main.js
@@ -1,10 +1,19 @@
+let nav = document.body.getElementsByClassName('top-nav')[0];
+let modal = document.body.getElementsByClassName('modal')[0];
+
 function openMobileMenu(){
-    let nav = document.body.getElementsByClassName('top-nav')[0];
     nav.style.left="0";
-    
+    modal.style.display="block";
 }//Fin funtion
 
 function closeMobileMenu(){
-    let nav = document.body.getElementsByClassName('top-nav')[0];
     nav.style.left="-24em";
+    modal.style.display="none";
+}//Fin funtion
+
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display="none";
+        nav.style.left="-24em";
+    }
 }
